d37d2dbe3a06baba2e8e969cc2e3cc5598eab20d
Anas Zughayyar <anas.ezzughayyar@gmail.com>
2024-12-09 13:33:44 +0200

    new

---
+++ src/Physician.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Physician extends User implements HIPAACompliantUser {
+	private ArrayList<String> patientNotes;
+	
+	// TO DO: Constructor that takes an IDcopy
+    public Physician(Integer id) {
+        this.id = id;
+    }
+    // TO DO: Implement HIPAACompliantUser!
+	
+	public void newPatientNotes(String notes, String patientName, Date date) {
+        String report = String.format(
+            "Datetime Submitted: %s \n", date);
+        report += String.format("Reported By ID: %s\n", this.id);
+        report += String.format("Patient Name: %s\n", patientName);
+        report += String.format("Notes: %s \n", notes);
+        this.patientNotes.add(report);
+    }
+
+    @Override
+    public boolean assignPin(int pin) {
+        return false;
+    }
+
+    @Override
+    public boolean accessAuthorized(Integer confirmedAuthID) {
+        return false;
+    }
+
+    // TO DO: Setters & Getters
+
+    public ArrayList<String> getPatientNotes() {
+        return patientNotes;
+    }
+
+    public void setPatientNotes(ArrayList<String> patientNotes) {
+        this.patientNotes = patientNotes;
+    }
+}
